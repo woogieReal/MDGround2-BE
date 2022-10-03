d27d1fb7dcfa13d6144815ccd6e2210e33ed6e76
woogieReal <woogiereal@gmail.com>
2022-10-03 17:30:55 +0900

    미들웨어 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import { AuthMiddleware } from './common/middleware/auth.middleware';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { TreesModule } from './trees/trees.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TreesModule, UsersModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware, LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
