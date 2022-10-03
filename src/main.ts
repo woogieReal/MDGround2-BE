577d80c91e142b770e1ed2b660a7c8909cc36341
woogieReal <woogiereal@gmail.com>
2022-10-03 22:39:35 +0900

    exception filter 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filter/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(8098);
 }
 bootstrap();
