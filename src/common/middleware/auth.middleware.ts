d66a864c39714f06b97cbb73295fed4c8090bb65
woogieReal <woogiereal@gmail.com>
2022-10-10 17:14:00 +0900

    미들웨어 로그 메시지 변경

--- src/common/middleware/auth.middleware.ts
+++ src/common/middleware/auth.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
+    console.log('Auth checking ...');
     next();
   }
 }
