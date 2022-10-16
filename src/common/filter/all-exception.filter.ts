41e2663a7bf78be868a68b07ceaa305e38fdc73c
woogieReal <woogiereal@gmail.com>
2022-10-16 15:41:25 +0900

    AllExceptionsFilter @Catch 데코레이터 인수 추가

--- src/common/filter/all-exception.filter.ts
+++ src/common/filter/all-exception.filter.ts
@@ -6,12 +6,13 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-@Catch()
+@Catch(HttpException)
 export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
+    console.log(exception);
     const status = exception.getStatus();
 
     response.status(status).json({
