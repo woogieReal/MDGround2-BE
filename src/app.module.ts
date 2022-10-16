1e9c7f3db7f9fcbbe238ca8cbf0a2d8932c738aa
woogieReal <woogiereal@gmail.com>
2022-10-16 12:42:26 +0900

    typeorm config 생성 및 root 모듈에 연경

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import {
   MiddlewareConsumer,
   RequestMethod,
 } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthMiddleware } from './common/middleware/auth.middleware';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { typeORMConfig } from './configs/typeorm.config';
 import { TreesModule } from './trees/trees.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TreesModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), TreesModule, UsersModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'md2',
+  entities: [__dirname + '/../**/*.entity.{js.ts}'],
+  synchronize: true,
+};
